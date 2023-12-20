b910ce38e12b2cfbb12bdb7f6515371e4ef40c5f
ishay.bentov <ishay.bentov@moovit.com>
2023-12-20 10:08:17 +0200

    increased turf version to fix test of area, in case moving to old version back need to change the area test

--- test/measurments.test.ts
+++ test/measurments.test.ts
@@ -1,5 +1,5 @@
 import * as measurements from '../src/measurements';
-import {Point} from '../src/classes';
+import {Point, Polygon} from '../src/classes';
 
 test('Haversine distance', () => {
   const from = new Point(0, 0);
@@ -11,4 +11,18 @@ test('Geodesic distance', () => {
   const from = new Point(0, 0);
   const to = new Point(1, 0);
   expect(measurements.geodesicDistance(from, to)).toBe(111319.49079327357);
+});
+
+test('Area of Australia polygon', () => {
+  const points = new Array<Point>();
+  points.push(new Point(125, -15));
+  points.push(new Point(113, -22));
+  points.push(new Point(117, -37));
+  points.push(new Point(130, -33));
+  points.push(new Point(148, -39));
+  points.push(new Point(154, -27));
+  points.push(new Point(144, -15));
+  points.push(new Point(125, -15));
+  const polygon = new Polygon(points);
+  expect(Math.round(measurements.area(polygon))).toBe(7748891609977);
 });
