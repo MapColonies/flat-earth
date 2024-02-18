1ae2584012b37cabd7476af8a9133ea38b35e94d
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-02-18 14:36:55 +0200

    docs: better func docs

--- src/tiles/tile_grids.ts
+++ src/tiles/tile_grids.ts
@@ -3,8 +3,9 @@ import { TileGrid } from './tiles_classes';
 
 /**
  * Check if the given location is on the edge of the tile grid
- * @param geoPoint
- * @param referenceTileGrid
+ * @param geoPoint point to check
+ * @param referenceTileGrid tile grid to check against
+ * @returns true/false if the `geoPoint` lies on tile grid's edge
  */
 export function isPointOnEdgeOfTileGrid(geoPoint: GeoPoint, referenceTileGrid: TileGrid): boolean {
   return geoPoint.lon === referenceTileGrid.boundingBox.max.lon || geoPoint.lat === referenceTileGrid.boundingBox.min.lat;
