cf63aaa16b24441c53b2e16f2a1c4fd122ace1bf
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-05-24 01:06:15 +0300

    feat: decode and encode crs from and to json

---
+++ src/crs/crs.ts
@@ -0,0 +1,9 @@
+import type { ConcreteCoordRefSys, ConcreteCoordRefSysJSON } from '../types';
+
+export function decodeFromJSON(coordRefSysJSON: ConcreteCoordRefSysJSON['coordRefSys']): ConcreteCoordRefSys['coordRefSys'] {
+  return typeof coordRefSysJSON === 'object' && 'wkt' in coordRefSysJSON ? { wkt: JSON.stringify(coordRefSysJSON.wkt) } : coordRefSysJSON;
+}
+
+export function encodeToJSON(coordRefSys: ConcreteCoordRefSys['coordRefSys']): ConcreteCoordRefSysJSON['coordRefSys'] {
+  return typeof coordRefSys === 'object' && 'wkt' in coordRefSys ? { wkt: JSON.parse(coordRefSys.wkt) as object } : coordRefSys;
+}
