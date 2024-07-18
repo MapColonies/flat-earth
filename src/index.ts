26da36b35e6ad8186c5f51bb92989ef82be36a75
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-07-18 23:01:02 +0300

    refactor: re-export all

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-export { BoundingBox } from './geometries/boundingBox';
-export { GeometryCollection } from './geometries/geometryCollection';
-export { Line } from './geometries/line';
-export { Point } from './geometries/point';
-export { Polygon } from './geometries/polygon';
-export { area, distance, geodesicDistance, geometriesEqual } from './measurements/measurements';
-export { Tile } from './tiles/tile';
-export { TileMatrixSet } from './tiles/tileMatrixSet';
+export * from './geometries/boundingBox';
+export * from './geometries/geometryCollection';
+export * from './geometries/line';
+export * from './geometries/point';
+export * from './geometries/polygon';
+export * from './measurements/measurements';
+export * from './tiles/tile';
+export * from './tiles/tileMatrixSet';
 
-export * from './types';
+export type * from './types';
