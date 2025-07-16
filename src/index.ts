751310766d5577df88ac04282fb27a07a63787b7
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-07-16 18:54:05 +0300

    fix: export public api only

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,50 @@
+export * from './geometries/baseGeometry';
 export * from './geometries/boundingBox';
+export * from './geometries/geometry';
 export * from './geometries/geometryCollection';
 export * from './geometries/line';
 export * from './geometries/point';
 export * from './geometries/polygon';
 export * from './measurements';
+export { CORNER_OF_ORIGIN_CODE } from './tiles/constants';
 export * from './tiles/tile';
 export * from './tiles/tileMatrixCollection';
+export * from './tiles/tileRange';
 
-export type * from './types';
+export type { ArrayElement } from './utils/types';
+export type {
+  BoundingBox2D,
+  CRS,
+  CodeType,
+  Comparison,
+  CornerOfOriginCode,
+  Keyword,
+  LanguageString,
+  Point2D,
+  TileEdgeInclusion,
+  TileIndex,
+  TileMatrix,
+  TileMatrixId,
+  TileMatrixJSON,
+  TileMatrixLimits,
+  TileMatrixSet,
+  TileMatrixSetJSON,
+  URI,
+  VariableMatrixWidth,
+} from './tiles/types';
+export type {
+  BoundingBoxInput,
+  CoordRefSysJSON,
+  GeoJSONBaseGeometry,
+  GeoJSONGeometry,
+  GeoJSONGeometryCollection,
+  GeoJSONLineString,
+  GeoJSONPoint,
+  GeoJSONPolygon,
+  GeometryCollectionInput,
+  JSONFG,
+  JSONFGFeature,
+  LineStringInput,
+  PointInput,
+  PolygonInput,
+} from './geometries/types';
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type { ArrayElement } from './utils/types';
-export type { Comparison } from './tiles/types';
-export type { TileEdgeInclusion } from './tiles/types';
-export type { CoordRefSys } from './geometries/types';
-export type { CoordRefSysJSON } from './geometries/types';
-export type { ConcreteCoordRefSys } from './geometries/types';
-export type { ConcreteCoordRefSysJSON } from './geometries/types';
