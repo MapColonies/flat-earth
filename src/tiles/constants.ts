9e0efd5604e62f5a8fd34d390073af9ccd609f1d
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-12-17 22:09:40 +0200

    docs: remove unsupported tags

--- src/tiles/constants.ts
+++ src/tiles/constants.ts
@@ -6,12 +6,10 @@ export const CORNER_OF_ORIGIN_CODE = ['topLeft', 'bottomLeft'] as const;
 
 /**
  * Tile Matrix Collection World CRS84 Quad, This Tile Matrix Collection defines tiles in the Equirectangular Plate Carrée projection in the CRS84 CRS for the whole world - https://docs.ogc.org/is/17-083r4/17-083r4.html#toc50
- * @category Tile Matrix Collection
  */
 export const TILEMATRIXCOLLECTION_WORLD_CRS84_QUAD = new TileMatrixCollection(TILEMATRIXSETJSON_WORLD_CRS84_QUAD);
 
 /**
  * Tile Matrix Collection Web Mercator Quad, This Tile Matrix Collection defines tiles based on a spherical Mercator instead of an ellipsoid - https://docs.ogc.org/is/17-083r4/17-083r4.html#toc49
- * @category Tile Matrix Collection
  */
 export const TILEMATRIXCOLLECTION_WEB_MERCATOR_QUAD = new TileMatrixCollection(TILEMATRIXSETJSON_WEB_MERCATOR_QUAD);
